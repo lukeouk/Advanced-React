5abc9224d30af1f84b35e2de7f1abe431bd3c105
Luke Jones <luke@lukeojones.com>
2020-01-29 19:01:57 +0000

    Implemented First Query and Mutation

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,15 @@
-const mutations = {};
+const mutations = {
+
+    async createItem(parent, args, ctx, info) {
+        // TODO: Checked if logged in
+        const item = await ctx.db.mutation.createItem({
+            data: {
+                ...args
+            }
+        }, info);
+
+        return item;
+    }
+};
 
 module.exports = mutations;
--- sick-fits/backend/src/resolvers/Query.js
+++ sick-fits/backend/src/resolvers/Query.js
@@ -1,3 +1,13 @@
-const Query = {};
+const { forwardTo } = require('prisma-binding');
+
+const Query = {
+    async items(parent, args, ctx, info) {
+        const items = await ctx.db.query.items();
+        return items;
+    }
+
+    //If you want to forward straight to prisma graphql
+    //items: forwardTo('db')
+};
 
 module.exports = Query;
